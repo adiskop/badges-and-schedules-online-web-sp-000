18447fe4d3b50a63cb7a4a43272cfc3fb7ef9033
Adi Skop <adiskop@gmail.com>
2020-06-28 08:23:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,6 +14,7 @@ def batch_badge_creator(speakers)
 end
 
 def assign_rooms(speakers)
+  rooms=[1,2,3,4,5,6,7]
   speakers.each do
     "Hello, #{{name}}! You'll be assigned to room #{{rooms}}!"
   end
