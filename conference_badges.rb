dd022fc2efe7e959daa8fdfbaf4b4c7b60ea0027
Adi Skop <adiskop@gmail.com>
2020-06-28 08:23:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,6 @@
 # Write your code here.
 speakers= ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+rooms=[1,2,3,4,5,6,7]
 
 def badge_maker(name)
   "Hello, my name is #{name}."
@@ -10,4 +11,12 @@ def batch_badge_creator(speakers)
     "Hello, my name is #{speaker}."
 
   end
+end
+
+def assign_rooms(speakers)
+  speakers.each do
+    "Hello, #{{name}}! You'll be assigned to room #{{rooms}}!"
+  end
+  
+  
 end
