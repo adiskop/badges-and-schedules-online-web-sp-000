4ba4c2bf87639d5ebe80e50086ec765adc388eeb
Adi Skop <adiskop@gmail.com>
2020-06-28 08:10:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,9 +5,8 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(speakers)
-  speakers.map do |each|
-    "Hello my name is  #{each}"
-
+def batch_badge_creator(attendees)
+  attendees.map do |attendee|
+    "Hello, my name is #{attendee}."
   end
 end
