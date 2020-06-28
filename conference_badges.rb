59a809eb7868363eb69bb081638fab6a895b57e2
Adi Skop <adiskop@gmail.com>
2020-06-28 07:35:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,5 @@
 # Write your code here.
-
-attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-
-def badge_maker(name)
-  "Hello, my name is #{name}."
+name= ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus","Matz"]
+def budge_maker(name)
+  puts "Hello, my name is #{name}."
 end
